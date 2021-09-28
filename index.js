f1ca3dba1bfa72eb1d604e7346b3fc66fc9ac077
Kostiantyn Smyrnov <kostysh@gmail.com>
2021-09-28 21:49:10 +0300

    added index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const org = require('./dist/org.json');
+const vc = require('./dist/vc.json');
+const nft = require('./dist/nft.json');
+const orgVcNft = require('./dist/orgVcNft.json');
+
+module.exports = {
+  org,
+  vc,
+  nft,
+  orgVcNft
+};
